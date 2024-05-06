9f7de00c6313442e9af1327ac38d000d961cb5e4
Rafał Zubala <rzubala@gmail.com>
2024-05-06 15:45:04 +0200

    dynamic pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,12 +1,39 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetailsPage() {
-    return (
-        <MeetupDetail 
-            image="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Trevi-Brunnen_abends.jpg/2880px-Trevi-Brunnen_abends.jpg" 
-            title="A First meetup"
-            address="some address 2, 12345 City"
-            description="The meetup description"
-            />
-    );
+export default function MeetupDetailsPage(props) {
+    return <MeetupDetail {...props.meetupData} />;
+}
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+    return {
+        props: {
+            meetupData: {
+                id: meetupId,
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Trevi-Brunnen_abends.jpg/2880px-Trevi-Brunnen_abends.jpg",
+                title: "A First meetup",
+                address: "some address 2, 12345 City",
+                description: "The meetup description",
+            },
+        },
+    };
+}
+
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: {
+                    meetupId: "m1",
+                },
+            },
+            {
+                params: {
+                    meetupId: "m2",
+                },
+            },
+        ],
+    };
 }
--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,26 @@ const DUMMY_MEETUPS = [
     },
 ]
 
-export default function HomePage() {
-    return (
-        <MeetupList meetups={DUMMY_MEETUPS} />
-    );
+export default function HomePage(props) {
+    return <MeetupList meetups={props.meetups} />;
+}
+
+// export async function getServerSideProps(context) {
+//     const req = context.req;
+//     const res = context.res;
+
+//     return {
+//         props: {
+//             meetups: DUMMY_MEETUPS
+//         }
+//     };
+// }
+
+export async function getStaticProps(context) {
+    return {
+        props: {
+            meetups: DUMMY_MEETUPS
+        },
+        revalidate: 1
+    };
 }
